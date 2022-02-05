c42180998802d1ed870a298a13c8176541961c90
declerckt <theo.declercks@hotmail.fr>
2022-02-05 06:20:07 +0100

    (:wrench:) little bit of cleaning

--- src/MhpImpl.java
+++ src/MhpImpl.java
@@ -9,15 +9,14 @@ public class MhpImpl {
     public static void main(String[] args){
 
         final int DEFAULT_LOOP = 10000;
-        final Random r = new Random();
         final DecimalFormat df = new DecimalFormat("0.00");
         long st = System.nanoTime();
-
 
         long ifYes[] = { 0 }; // Number of victories if you changed your decision after option is discovered
         long ifNo[] = { 0 }; // Number of victories if you did not change your decision after option was discovered
 
         long loopArgs;
+
         try{
             loopArgs = Long.parseLong(args[0]);
         }catch(NumberFormatException | ArrayIndexOutOfBoundsException e){
@@ -25,40 +24,40 @@ public class MhpImpl {
         }
 
         final long nbLoop = (loopArgs == -1) ? DEFAULT_LOOP : loopArgs;
+        
         LongStream.range(0,nbLoop).parallel().forEach(i-> {
+
             int v; // The simulated choice that makes you win
             int c; // The simulated users's choice
             int o; // The simulated option that is discovered after your choice (o!=v && o!=c)
             int nc; // The option that is left after the first option is discovered
+            v = ThreadLocalRandom.current().nextInt(3) + 1;
+            c = ThreadLocalRandom.current().nextInt(3) + 1;
 
-                        v = ThreadLocalRandom.current().nextInt(3) + 1;
-                        c = ThreadLocalRandom.current().nextInt(3) + 1;
+            if (v == c) {
+                boolean randomDoor = ThreadLocalRandom.current().nextBoolean();
+                if (v == 1)
+                    o = randomDoor ? 2 : 3;
+                else if (v == 2)
+                    o = randomDoor ? 1 : 3;
+                else
+                    o = randomDoor ? 1 : 2;
+            } else {
+                if ((v + c) == 3)
+                    o = 3;
+                else
+                    o = Math.abs(v - c);
+            }
 
-                        if (v == c) {
-                            boolean randomDoor = ThreadLocalRandom.current().nextBoolean();
-                            if (v == 1)
-                                o = randomDoor ? 2 : 3;
-                            else if (v == 2)
-                                o = randomDoor ? 1 : 3;
-                            else
-                                o = randomDoor ? 1 : 2;
-                        } else {
-                            if ((v + c) == 3)
-                                o = 3;
-                            else {
-                                o = Math.abs(v - c);
-                            }
-                        }
+            nc = 6 - o - c;
 
-                        nc = 6 - o - c;
+            if (o == v)
+                System.out.println("o==v");
 
-                        if (o == v)
-                            System.out.println("o==v");
-
-                        if (nc == v)
-                           ifYes[0]++;
-                        else if (c == v)
-                            ifNo[0]++;
+            if (nc == v)
+               ifYes[0]++;
+            else if (c == v)
+                ifNo[0]++;
         });
 
         System.out.println("");
